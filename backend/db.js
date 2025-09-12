0653473b17cc58080209bbef652efa2dd557d70f
harishadepu <harishadepu40@gmail.com>
2025-09-12 16:13:37 +0530

    chang

--- backend/db.js
+++ backend/db.js
@@ -6,8 +6,7 @@ const db = mysql.createConnection({
     host:process.env.host,
     user:process.env.user,
     password:process.env.password,
-    database:process.env.database,
-    connectTimeout: 10000
+    database:process.env.database
 })
 db.connect((err)=>{
     if (err) {
